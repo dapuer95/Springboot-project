361f3c5ab07aef8d9ad700eb76c7ed738c9fef70
dapuer95 <dpuerta1095@gmail.com>
2021-06-21 13:49:29 -0500

    Manejo de roles

--- src/main/java/com/proyecto/controlador/SecurityConfig.java
+++ src/main/java/com/proyecto/controlador/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.proyecto.controlador;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -22,5 +23,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.roles("USER")
 		;
 	}
-
+	
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+	
+		http.authorizeRequests().antMatchers("/editar/**", "/agregar/**", "/eliminar")
+		.hasRole("ADMIN")
+		.antMatchers("/", "/home")
+		.hasAnyRole("USER", "ADMIN")
+		.and()
+		.formLogin()
+		.loginPage("/login")
+		.and()
+		.exceptionHandling().accessDeniedPage("/errores/403")
+		;
+	}
 }
